fb45c79b40488459f477c0e52f06560c028d7681
guineveresaenger <guineveresaenger@yahoo.com>
2016-11-15 15:54:54 -0800

    Added most of the functionality for highestScoreFrom

--- scrabble.js
+++ scrabble.js
@@ -22,7 +22,6 @@ Scrabble.prototype.score = function(word) {
       case ("aeioulnrst".indexOf(word[i]) >= 0):
         total += 1;
         break;
-
       case ("dg".indexOf(word[i]) >=0):
         total += 2;
         break;
@@ -44,16 +43,66 @@ Scrabble.prototype.score = function(word) {
       default: {
         return 'Sorry, but this word contains an invalid character.';
       }
-
     }
-
   }
   return total;
+};
+
+
+Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
+
+  console.log("HI HELLO NEW FUNCTION");
+  console.log(this.score("jj"));
+
+  var scores = {};
+
+  // Assign scores to each word
+  for (var i = 0; i < arrayOfWords.length; i++) {
+    scores[arrayOfWords[i]] = this.score(arrayOfWords[i]);
+  }
+  console.log(scores);
+  console.log(Object.values(scores));
+
+  // get largest score regardless of duplicates
+  function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
+  }
+
+  var maxScore = getMaxOfArray(Object.values(scores));
+
+  // get all poential winning words
+  var potentialWinners = [];
+
+  for (var word in scores) {
+    if(scores.hasOwnProperty(word)) {
+      console.log("This is the word: " + word + " and the score: " + scores[word]);
+      if (scores[word] == maxScore) {
+        console.log("A winner is: " + word);
+        potentialWinners.push(word);
+        console.log(potentialWinners);
+      }
+    }
+  }
+
+  // go through winners array
+  if (potentialWinners.length == 1) {
+    return potentialWinners[0];
+  }
+  else {
+    for (var j = 0; j < potentialWinners.length; j++) {
+      if (potentialWinners[j].length == 7) {
+        return potentialWinners[j];
+      }
+    }
+    // TODO: return shortest word!
+
+  }
 
 };
 
 game = new Scrabble();
 console.log(game.helloWorld());
-console.log(game.score("zzz"));
+// console.log(game.score("zzz"));
+game.highestScoreFrom(["zzz", "qqq", "word", "hi"]);
 
 module.exports = Scrabble;
