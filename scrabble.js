23b51663905c6b2eeeeb702fcf812241e2ad2de8
guineveresaenger <guineveresaenger@yahoo.com>
2016-11-16 11:18:28 -0800

    Made Player object with full functionality

--- scrabble.js
+++ scrabble.js
@@ -78,8 +78,10 @@ Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
   }
   else {
     // return seven-letter word if there is one (we assume there's only one...)
+    console.log(potentialWinners);
     for (var j = 0; j < potentialWinners.length; j++) {
-      if (potentialWinners[j].length == 7) {
+      console.log(potentialWinners[j].length);
+      if (potentialWinners[j].length === 7) {
         return potentialWinners[j];
       }
     }
@@ -98,19 +100,90 @@ Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
 
 };
 
-var Player = function() {};
+var Player = function(name) {
+  this.name = name;
+  this.plays = [];
+  this.game = new Scrabble();
+};
 
 Player.prototype.helloWorld = function () {
   return "Hi I'm playing Scrabble.";
 };
 
+Player.prototype.play = function(word) {
+  if (this.hasWon() === true) {
+    return false;
+  }
+  else {
+    return this.plays.push(word);
+  }
+};
+
+Player.prototype.totalScore = function() {
+  // score all words in plays
+  var totalScore = 0;
+  for (var i = 0; i < this.plays.length; i ++) {
+    totalScore += this.game.score(this.plays[i]);
+  }
+  return totalScore;
+
+};
+
+Player.prototype.hasWon = function() {
+  console.log("Here is the total score: " + this.totalScore());
+  if (this.totalScore() >= 100) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+Player.prototype.highestScoringWord = function() {
+  return this.game.highestScoreFrom(this.plays);
+};
+
+Player.prototype.highestWordScore = function() {
+  return this.game.score(this.highestScoringWord());
+}; 
 
 game = new Scrabble();
 console.log(game.helloWorld());
-// console.log(game.score("zzz"));
-console.log(game.highestScoreFrom(["xxxbb", "zzz", "qqq", "word", "hi"]));
 
-player = new Player();
-console.log(player.helloWorld());
+console.log(game.highestScoreFrom(["xxxbb", "kkkkkkk", "zzz", "qqq", "word", "hi"]));
+
+// player = new Player("Guin");
+// console.log(player.helloWorld());
+// console.log(player.name);
+// console.log(player.plays);
+// player.play("Hello");
+// console.log(player.game.score("Hello"));
+// console.log(player.plays);
+// player.play("Elefant");
+// console.log(player.game.score("Elefant"));
+// console.log(player.plays);
+// player.play("zzz");
+// console.log(player.game.score("zzz"));
+// console.log(player.plays);
+// console.log(player.totalScore());
+// console.log(player.hasWon());
+// console.log("******************");
+// player.play("zzz");
+// console.log(player.plays);
+// console.log(player.totalScore());
+// console.log(player.hasWon());
+// console.log("******************");
+// player.play("zzz");
+// console.log(player.plays);
+// console.log(player.totalScore());
+// console.log(player.hasWon());
+// console.log("******************");
+// player.play("zzz");
+// console.log(player.plays);
+// console.log(player.totalScore());
+// console.log(player.hasWon());
+// console.log(player.highestScoringWord());
+// console.log(player.highestWordScore());
+
 
 module.exports = Scrabble;
