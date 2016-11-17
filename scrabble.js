fca141e0ecccd557aaeec249f72f451b5fe78b9d
guineveresaenger <guineveresaenger@yahoo.com>
2016-11-17 10:28:34 -0800

    fixed the Board, and added a function to actually play the tiles.

--- scrabble.js
+++ scrabble.js
@@ -1,11 +1,11 @@
-var Scrabble = function() {};
+var Scrabble = {};
 
 // YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
+Scrabble.helloWorld = function() {
   return 'hello world!';
 };
 
-Scrabble.prototype.score = function(word) {
+Scrabble.score = function(word) {
   word = word.toLowerCase();
   var total = 0;
   var scoreChart = {
@@ -30,7 +30,7 @@ Scrabble.prototype.score = function(word) {
   return total;
 };
 
-Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
+Scrabble.highestScoreFrom = function(arrayOfWords) {
 
   var scores = {};
 
@@ -81,7 +81,6 @@ Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
 var Player = function(name) {
   this.name = name;
   this.plays = [];
-  this.game = new Scrabble();
 };
 
 Player.prototype.helloWorld = function () {
@@ -101,7 +100,7 @@ Player.prototype.totalScore = function() {
   // score all words in plays
   var totalScore = 0;
   for (var i = 0; i < this.plays.length; i ++) {
-    totalScore += this.game.score(this.plays[i]);
+    totalScore += Scrabble.score(this.plays[i]);
   }
   return totalScore;
 
@@ -118,11 +117,11 @@ Player.prototype.hasWon = function() {
 };
 
 Player.prototype.highestScoringWord = function() {
-  return this.game.highestScoreFrom(this.plays);
+  return Scrabble.highestScoreFrom(this.plays);
 };
 
 Player.prototype.highestWordScore = function() {
-  return this.game.score(this.highestScoringWord());
+  return Scrabble.score(this.highestScoringWord());
 };
 
 
@@ -134,30 +133,33 @@ var Board = function(){
 Board.prototype.newBoard = function() {
   // a Scrabble board is 15x15
   var newBoard = [];
-  var line = [];
-  for (var i = 0; i < 15; i++) {
-    line.push(null);
-  }
   for (var j = 0; j < 15; j++) {
+    var line = [];
+    for (var i = 0; i < 15; i++) {
+      line.push(null);
+    }
     newBoard.push(line);
   }
   return newBoard;
 };
 
 Board.prototype.checkAvailability = function(word, startSquare, direction) {
-  var wordLength = word.length;
   // check if there's a contiguous amount of null values in each line
   // e.g startSquare [0,0] - top left corner
-  // with direction "across" TODO: check validity of direction
+  // check valid direction input
+  if (direction !== "across" && direction !== "down") {
+    return "Your word can only be played 'across' or 'down'.";
+  }
+
   if (direction === "across") {
-    for (var i = 0; i < wordLength; i++) {
-      if ( this.board[startSquare[0]][startSquare[1] + i] !== null) {
+    for (var i = 0; i < word.length; i++) {
+      if ((startSquare[1] + i) > 14 || this.board[startSquare[0]][startSquare[1] + i] !== null) {
         return false;
       }
     }
   }
   else if (direction === "down") {
-    for (var j = 0; j < wordLength; j++) {
+    for (var j = 0; j < word.length; j++) {
       if ((startSquare[0] + j) > 14 || this.board[startSquare[0] + j][startSquare[1]] !== null) {
         return false;
       }
@@ -166,5 +168,20 @@ Board.prototype.checkAvailability = function(word, startSquare, direction) {
   return true;
 };
 
+Board.prototype.playWord = function(word, startSquare, direction) {
+  if (this.checkAvailability(word, startSquare, direction) === true){
+    if (direction === "across") {
+      for (var i = 0; i < word.length; i++) {
+        this.board[startSquare[0]][startSquare[1] + i] = word[i];
+      }
+    }
+    else if (direction === "down") {
+      for (var j = 0; j < word.length; j++) {
+        this.board[startSquare[0] + j][startSquare[1]] = word[j];
+      }
+    }
+  }
+  return this.board;
+};
 
 module.exports = Scrabble;
